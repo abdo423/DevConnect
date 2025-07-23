53b6b1b60c44cacb136044d17d72832bf80366bd
abdo423 <man-body@hotmail.com>
2025-07-24 01:12:35 +0300

    refactor(routes): consolidate backend routing using Express Router pattern
    Reorganized all route files to consistently use the Express Router pattern.
    Separated protected and unprotected routes and registered them in index.ts
    to improve maintainability and scalability.

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -3,15 +3,17 @@ dotenv.config();
 import express from 'express';
 import config from 'config';
 import mongoose from "mongoose";
-import userRoutes from './routes/auth';
+import authRoutes from './routes/auth';
 import cookieParser from 'cookie-parser';
 import cors from "cors";
 import {Request, Response, NextFunction} from "express";
 import commentRoutes from "./routes/comment";
-import authCheck, {authMiddleware, checkTokenExpiration} from "./middlewares/auth";
+import  {authMiddleware, checkTokenExpiration} from "./middlewares/auth";
 import postRoutes from "./routes/Post";
 import profileRoutes from "./routes/Profile";
 import messageRoutes from "./routes/Message";
+import protectedRoutes from "./routes/protectedAuth";
+import protectedPost from "./routes/protectedPost";
 const app = express();
 app.use(express.json({limit: '10mb'}));
 app.use(cookieParser());
@@ -36,8 +38,10 @@ app.get("/", (req: Request, res: Response) => {
 app.use(express.urlencoded({extended: true, limit: '10mb'}));
 
 //routes
-userRoutes(app, authMiddleware);
-postRoutes(app, authMiddleware);
+app.use("/Auth", authRoutes);
+app.use("/Auth", authMiddleware,protectedRoutes);
+app.use("/Post", postRoutes);
+app.use("/Post", authMiddleware,protectedPost);
 app.use("/Comment", authMiddleware, commentRoutes);
 app.use("/Profile", authMiddleware, profileRoutes);
 app.use("/Message", authMiddleware, messageRoutes);
--- Server/src/routes/Post.ts
+++ Server/src/routes/Post.ts
@@ -20,33 +20,5 @@ router.use((req, res, next) => {
 router.get("/all", (req: Request, res: Response) => {
     getPosts(req, res);
 });
+export default router;
 
-export default (app: any, authMiddleware: any) => {
-    // Mount the public route
-    app.use("/Post", router);
-
-    // Protected routes
-    const protectedRoutes = Router();
-
-    protectedRoutes.post("/create", (req: Request, res: Response) => {
-        createPost(req, res);
-    });
-
-    protectedRoutes.delete("/delete/:id", (req: Request, res: Response) => {
-        deletePost(req, res);
-    });
-
-    protectedRoutes.patch("/update/:id", (req: Request, res: Response) => {
-        updatePost(req, res);
-    });
-
-    protectedRoutes.post("/like/:id", (req: Request, res: Response) => {
-        likePost(req, res);
-    });
-
-    protectedRoutes.get("/comments/:id", (req: Request, res: Response) => {
-        getCommentsByPostIdArray(req, res);
-    });
-
-    app.use("/Post", authMiddleware, protectedRoutes);
-};
